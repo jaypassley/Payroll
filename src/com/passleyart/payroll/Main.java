9d918ebdd475745b5ae299dd6dc4697d4612cfd5
jaypassley <jaypassley@yahoo.com>
2022-03-16 05:02:15 -0400

    successfully implemented & displayed salaried employee

--- src/com/passleyart/payroll/Main.java
+++ src/com/passleyart/payroll/Main.java
@@ -1,8 +1,34 @@
 package com.passleyart.payroll;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+
+        Scanner sc = new Scanner(System.in);
+
+        SalariedEmployee SE = new SalariedEmployee();
+
+        //Enter input for Salaried Employee
+        System.out.println("\nEnter Payroll Info For a Salaried Employee\n******************************************\n");
+
+        System.out.println("Enter First Name");
+
+        SE.setFirstName(sc.next());
+
+        System.out.println("Enter Last Name");
+        SE.setLastName(sc.next());
+
+        System.out.println("Enter Social Security #");
+        SE.setSocialSecurityNumber(sc.next());
+
+        System.out.println("Enter Weekly Salary");
+        SE.setWeeklySalary(sc.nextDouble());
+
+
+        //Display Manger info
+        System.out.println("\nSalaried Employee Info\n");
+        SE.display();
+}
 }
--- src/com/passleyart/payroll/SalariedEmployee.java
+++ src/com/passleyart/payroll/SalariedEmployee.java
@@ -4,11 +4,24 @@ public class SalariedEmployee extends Employee{
 
     private double weeklySalary;
 
+    public double getPaymentAmount() {
+        return getWeeklySalary();
+    }
+
+
     public double getWeeklySalary() {
         return weeklySalary;
     }
 
     public void setWeeklySalary(double weeklySalary) {
         this.weeklySalary = weeklySalary;
+    }
+
+    public void display() {
+        System.out.println("First Name :" + getFirstName() + "\n" +
+                "Last Name: " + getLastName() + "\n" +
+                "Social Security #: " + getSocialSecurityNumber() + "\n" +
+                "Weekly Salary: " + getWeeklySalary() + "\n" +
+                "Payment Amount: " + getPaymentAmount() + "\n");
     }
 }
